31fe63e5dec617e21da96d39c66aefc4cf2e1636
hotdudehvfun <hotdudehvfun@gmail.com>
2018-08-29 17:49:26 +0530

    Add files via upload

---
+++ savedata.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "den1.mysql6.gear.host:3306";
+$username = "mydb40";
+$password = "123456#";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+$key = $_POST['key'];
+print_r(json_encode($_POST));
+
+?>
