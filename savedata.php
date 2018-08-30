bedef95661bb970476e718369bfa336177041ff6
hotdudehvfun <hotdudehvfun@gmail.com>
2018-08-30 17:53:52 +0530

    Update savedata.php

--- savedata.php
+++ savedata.php
@@ -9,27 +9,34 @@ $conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error)
 {
-     
-echo "problem with connection";
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully";
 
 
-
-print_r(json_decode($_POST));
+try
+{
+	// $paradeDate = $_POST['paradeDate'];
+// $paradeCoy = $_POST['paradeCoy'];
+// $paradeHtml = $_POST['paradeHtml'];
+// $paradeJson = $_POST['paradeJson'];
 //insert into parade (paradeDate,paradeCoy,paradeHtml,paradeJson) values("2018-1-1","a","html","json");
 
 // $stmt=$conn->prepare("insert into parade (paradeDate,paradeCoy,paradeHtml,paradeJson) values(?,?,?,?)");
+$stmt=$conn->prepare("insert into parade (paradeDate,paradeCoy,paradeHtml,paradeJson) values('2018-1-1','a','html','json')");
 // $stmt->bind_param("ssss", $paradeDate, $paradeCoy, $paradeHtml,$paradeJson);
 // $paradeDate = $_POST['paradeDate'];
 // $paradeCoy = $_POST['paradeCoy'];
 // $paradeHtml = $_POST['paradeHtml'];
 // $paradeJson = $_POST['paradeJson'];
-// $stmt->execute();
+$stmt->execute();
+$result=$stmt->affected_rows;
+$stmt->close();
+$conn->close();
+echo "Affected rows:".$result;
 
-// $result=$stmt->affected_rows;
-// $stmt->close();
-// $conn->close();
-// echo "Affected rows:".$result;
+}catch(Exception $e)
+{
+	  echo 'Message: ' .$e->getMessage();
+}
 ?>
