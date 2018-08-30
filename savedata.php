9aa1741ccdbbfdd325473c7f06a5cedcd6f2e41f
hotdudehvfun <hotdudehvfun@gmail.com>
2018-08-30 20:00:06 +0530

    Update savedata.php

--- savedata.php
+++ savedata.php
@@ -4,33 +4,27 @@ $username = "mydb40";
 $password = "123456#";
 $db = "mydb40";
 
-
 // Create connection
-$conn = mysqli_connect($servername, $username, $password,$db);
+$conn = new mysqli($servername, $username, $password,$db);
 
 // Check connection
-if (mysqli_connect_errno())
+if ($conn->connect_error)
 {
-    die("Connection failed: " .mysqli_connect_errno());
+    die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+echo "Connected successfully 1";
 
+$sql = "INSERT INTO parade (paradeDate, paradeCoy, paradeHtml,paradeJson)
+VALUES ('2018-1-1', 'D', 'html','json')";
 
-try
+if ($conn->query($sql) === TRUE)
 {
-	echo "sql try";
-	// $paradeDate = $_POST['paradeDate'];
-	// $paradeCoy = $_POST['paradeCoy'];
-	// $paradeHtml = $_POST['paradeHtml'];
-	// $paradeJson = $_POST['paradeJson'];
-	//insert into parade (paradeDate,paradeCoy,paradeHtml,paradeJson) values("2018-1-1","a","html","json");
-
-	// $stmt=$conn->prepare("insert into parade (paradeDate,paradeCoy,paradeHtml,paradeJson) values(?,?,?,?)");
-	mysqli_query($con,"show tables;");
-	echo "Affected rows: " . mysqli_affected_rows($con);
-	mysqli_close($con);
-}catch(Exception $e)
-{
-	  echo 'Message: ' .$e->getMessage();
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+
+$conn->close();
+
 ?>
