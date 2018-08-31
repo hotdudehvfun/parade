0602a738b0957b2022e03ac54ef1849d529a2602
hotdudehvfun <hotdudehvfun@gmail.com>
2018-08-31 18:14:43 +0530

    Update savedata.php

--- savedata.php
+++ savedata.php
@@ -25,16 +25,57 @@ $paradeCoy=$_POST['paradeCoy'];
 $paradeHtml=$_POST['paradeHtml'];
 $paradeJson=$_POST['paradeJson'];
 
-$sql = "INSERT INTO parade (paradeDate, paradeCoy, paradeHtml,paradeJson) VALUES "."( '".$paradeDate."',".  "'".$paradeCoy."',".  "'".$paradeHtml."'," . "'".$paradeJson."')";
+
+//check if date and coy exists
+
+$sql="SELECT * FROM parade WHERE paradeDate=". "'".$paradeDate."'" ."AND paradeCoy="."'".$paradeCoy."'";
 
 if ($conn->query($sql) === TRUE)
 {
-	$echoArray['success_msg']="Parade statement successfully sent!!!";
-	$echoArray['success']=true;
-} else
+	//query executed 
+	if ($conn->affected_rows>0)
+	{
+		//previous statement exists update here
+		$sql = "UPDATE parade SET ";
+		$sql.=" paradeDate="."'".$paradeDate."'";
+		$sql.=" ,paradeCoy="."'".$paradeCoy."'";
+		$sql.=" ,paradeHtml="."'".$paradeHtml."'";
+		$sql.=" ,paradeJson="."'".$paradeJson."'";
+		$sql.=" WHERE ";
+		$sql.=" paradeDate="."'".$paradeDate."'";
+		$sql.=" AND ";
+		$sql.=" paradeCoy="."'".$paradeCoy."'";
+
+		if ($conn->query($sql) === TRUE)
+		{
+			$echoArray['success_msg']="Parade statement updated successfully!!!";
+			$echoArray['success']=true;
+		} else
+		{
+		    $echoArray['error_msg']="Error: " . $sql . "<br>" . $conn->error;
+		    $echoArray['success']=false;
+		}
+	}else
+	{
+		//no previous record insert new statement
+		$sql = "INSERT INTO parade (paradeDate, paradeCoy, paradeHtml,paradeJson) VALUES "."( '".$paradeDate."',".  "'".$paradeCoy."',".  "'".$paradeHtml."'," . "'".$paradeJson."')";
+
+		if ($conn->query($sql) === TRUE)
+		{
+			$echoArray['success_msg']="Parade statement sent successfully !!!";
+			$echoArray['success']=true;
+		} else
+		{
+		    $echoArray['error_msg']="Error: " . $sql . "<br>" . $conn->error;
+		    $echoArray['success']=false;
+		}
+	}
+}
+else
 {
-    $echoArray['error_msg']="Error: " . $sql . "<br>" . $conn->error;
-    $echoArray['success']=false;
+	//query did not execute
+	$echoArray['error_msg']="Error: " . $sql . "<br>" . $conn->error;
+    $echoArray['success']=false;	
 }
 $conn->close();
 echo json_encode($echoArray);
