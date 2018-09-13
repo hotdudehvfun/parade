7d0c95d9ff7848ee17fa4062671f4a446b262b74
hotdudehvfun <hotdudehvfun@gmail.com>
2018-09-13 21:40:41 +0530

    Update loadLiveFeeds.php

--- loadLiveFeeds.php
+++ loadLiveFeeds.php
@@ -4,17 +4,13 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 $dom = new DomDocument();
 $dom->load('live_feed.xml');
-
-// // Apply some modification
-// $parent = $dom->getElementsByTagName('live_feed')->item(0);
-// $node = $dom->createElement("feed","value");
-// $parent->appendChild($node);
-// // Save the new version of the file
-// $dom->saveXML();
-
 $feeds = $dom->getElementsByTagName('feed');
+$echoArray=array();
+$i=0;
 foreach ($feeds as $feed)
 {
-	echo $feed->nodeValue;
-} 
+	$echoArray[$i++]=$feed->nodeValue;
+	//echo $feed->nodeValue;
+}
+echo json_encode($echoArray);
 ?>
