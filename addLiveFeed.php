dd08e933b5d0c980bf33ef95e084ea1b5fbcd05f
hotdudehvfun <hotdudehvfun@gmail.com>
2018-09-13 22:39:08 +0530

    Update addLiveFeed.php

--- addLiveFeed.php
+++ addLiveFeed.php
@@ -4,10 +4,14 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 $dom = new DomDocument();
 $dom->load('live_feed.xml');
+
+$value=$_POST['value'];
+
 // Apply some modification
 $parent = $dom->getElementsByTagName('live_feed')->item(0);
-$node = $dom->createElement("feed","value");
+$node = $dom->createElement("feed",$value);
 $parent->appendChild($node);
 // Save the new version of the file
-$dom->save('live_feed.xml');
+$result=$dom->save('live_feed.xml');
+$echo $result;
 ?>
