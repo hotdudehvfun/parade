4196222391bb190b1b66ca05fac79572592d0269
hotdudehvfun <hotdudehvfun@gmail.com>
2018-09-13 21:35:10 +0530

    Create loadLiveFeeds.php

---
+++ loadLiveFeeds.php
@@ -0,0 +1,20 @@
+<?php 
+// open and load a XML file
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+$dom = new DomDocument();
+$dom->load('live_feed.xml');
+
+// // Apply some modification
+// $parent = $dom->getElementsByTagName('live_feed')->item(0);
+// $node = $dom->createElement("feed","value");
+// $parent->appendChild($node);
+// // Save the new version of the file
+// $dom->saveXML();
+
+$feeds = $dom->getElementsByTagName('feed');
+foreach ($feeds as $feed)
+{
+	echo $feed->nodeValue;
+} 
+?>
