e389a1f08028ac49a52c7128e851aff367e53c6d
hotdudehvfun <hotdudehvfun@gmail.com>
2018-09-15 17:03:36 +0530

    Update addLiveFeed.php

--- addLiveFeed.php
+++ addLiveFeed.php
@@ -2,15 +2,31 @@
 // open and load a XML file
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-$dom = new DomDocument();
-$dom->load('live_feed.xml');
 
-$value=$_POST['value'];
+$servername = "den1.mysql6.gear.host:3306";
+$username = "mydb40";
+$password = "123456#";
+$db = "mydb40";
 
-// Apply some modification
-$parent = $dom->getElementsByTagName('live_feed')->item(0);
-$child=$dom->createElement("feed",$value);
-$parent->appendChild($child);
-echo $dom->save('live_feed.xml');
+// Create connection
+ $conn = mysqli_connect($servername, $username, $password,$db);
 
+// Check connection
+if (!$conn)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$value="testing";//$_POST["value"];
+
+$sql="insert into feeds(feed,state) values('".$value."',true)";
+$result = mysqli_query($conn, $sql);
+if ($result)
+{
+	$echoArray['success']=true;			
+}else
+{
+	$echoArray['success']=false;
+}
+echo json_encode($echoArray);
 ?>
