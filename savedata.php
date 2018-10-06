d41a556e2677d8891ae61fc72f14969d52ec3d60
hotdudehvfun <hotdudehvfun@gmail.com>
2018-10-06 18:09:47 +0530

    Update savedata.php

--- savedata.php
+++ savedata.php
@@ -64,6 +64,7 @@ $result = mysqli_query($conn, $sql);
 	 //insert new row
 	 //$sql = "INSERT INTO parade (paradeDate, paradeCoy, paradeHtml,paradeJson) VALUES "."( '".$paradeDate."',".  "'".$paradeCoy."',".  "'".$paradeHtml."'," . "'".$paradeJson."')";
 	 $sql="INSERT INTO parade (paradeDate, paradeCoy, paradeHtml,paradeJson) VALUES('$paradeDate', '$paradeCoy', '$paradeHtml','$paradeJson')";
+	$echoArray["sql"]=$sql;
 	$result = mysqli_query($conn, $sql);
 	 $echoArray["result"]=$result;
 			if (mysqli_num_rows($result)>0)
